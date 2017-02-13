40a432a84475f3cb3d357c1ba77497af9d0d34ff
carlosmartell97 <carlosmartell97@gmail.com>
2017-02-12 19:39:32 -0600

    camera chases cube. Cube moves and jumps with arrows

---
+++ files/Assets/camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+	public GameObject character;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = character.transform.position+offset;
+	}
+}
---
+++ files/Assets/mainCharacter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCharacter : MonoBehaviour {
+
+	public int xVelocity, yJump;
+	public Rigidbody r;
+
+	// Use this for initialization
+	void Start () {
+		//r = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		float jump = Input.GetAxis ("Jump");
+		transform.Translate (horizontal * xVelocity * Time.deltaTime, 0, 0,Space.World);
+
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			r.AddRelativeForce(0, yJump, 0, ForceMode.Impulse);
+		}
+	}
+
+	void OnCollisionEnter(Collision c){
+		Debug.Log("with "+c.gameObject.name);
+	}
+}
