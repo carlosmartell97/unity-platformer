a79270ff5b630d69144e8971c4653014528f415a
chuyalvarez <jesusaagomez@hotmail.com>
2017-02-22 14:05:43 -0600

    added movable objects and enpoint objective

--- files/Assets/mainCharacter.cs
+++ files/Assets/mainCharacter.cs
@@ -5,33 +5,72 @@ using UnityEngine;
 public class mainCharacter : MonoBehaviour {
 
 	public int xVelocity, yJump;
-	public Rigidbody r;
-	bool jump;
-
+	Rigidbody r;
+	bool jump,grab, grabavb;
+	GameObject item;
+	int side;
 	// Use this for initialization
 	void Start () {
-		//r = GetComponent<Rigidbody> ();
+		r = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float horizontal = Input.GetAxis ("Horizontal");
-		float vertical = Input.GetAxis ("Vertical");
 		transform.Translate (horizontal * xVelocity * Time.deltaTime, 0, 0,Space.World);
 
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
+		if (horizontal > 0) {
+			side = 1;
+		}else if (horizontal < 0) {
+			side = -1;
+		}
+
+		if(Input.GetKeyDown(KeyCode.W)){
 			if (jump) {
 				r.AddRelativeForce (0, yJump, 0, ForceMode.Impulse);
 				jump = false;
 			}
 		}
+		if (Input.GetKeyDown (KeyCode.E)) {
+			Debug.Log ("e");
+			if (grabavb) {
+				grab = true;
+				Debug.Log (grab);
+			}
+		}
+
+		if (grab) {
+			item.transform.position = new Vector3(transform.position.x+(2*side),transform.position.y+1,transform.position.z);
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			grab = false;
+		}
 	}
 
 	void OnCollisionEnter(Collision c){
-		Debug.Log("with "+c.gameObject.name);
-
 		if (c.gameObject.name [0] == 'F') {
 			jump = true;
+		}
+			
+		if (c.gameObject.name == "Box") {
+			grabavb = true;
+			item = c.gameObject;
+			jump = true;
+		}
+
+		if (c.gameObject.name == "Flag") {
+			grabavb = true;
+			item = c.gameObject;
+		}
+	}
+
+	void OnCollisionExit(Collision c){
+		if (c.gameObject.name == "Box") {
+			grabavb = false;
+		}
+		if (c.gameObject.name == "Flag") {
+			grabavb = false;
 		}
 	}
 }
